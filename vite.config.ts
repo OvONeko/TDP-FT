26e7afd6962784b6efdaea4b5f924a0c5bf6c4e7
Elihuso Quigley <Elihuso@outlook.com>
2024-07-01 12:23:41 +0800

    [+] unplugin components

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import {fileURLToPath, URL} from 'node:url'
+import Components from 'unplugin-vue-components/vite'
+
+import {defineConfig} from 'vite'
 import markdown from './vite/markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), markdown()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: '/TDP-FT/'
+    plugins: [vue(), vueJsx(), markdown(), Components({ dts: true, dirs: ['src/components', 'src/views'] })],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    base: '/TDP-FT/'
 })
